dc20728cbbac200ac78a7cf9ab6b9c783ef8131e
BallyB <ajeanne25@gmail.com>
2014-12-15 15:13:43 +0100

    Modif avant manger

--- Modele/Etudiant.cpp
+++ Modele/Etudiant.cpp
@@ -2,26 +2,8 @@
 #include <iostream> // pour std::cout 
 #include <string>   // pour std::string 
 #include "Etudiant.h"
-
+#include "Personne.h"
 using namespace std;
 
-      std::string nom;
-      std::string prenom;
-      
-      std::string getNom(){
-             return nom; 
-       }      
-      
-      void setNom(std::string chaine){
-             nom = chaine;
-      }
-      
-      std::string getPrenom(){
-             return prenom;
-      }
-      
-      void setPrenom(std::string chaine){
-           prenom = chaine;
-      }
 
 
